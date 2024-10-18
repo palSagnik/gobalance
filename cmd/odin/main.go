6178f499aa909dd23faf034c21d66ccf0f2cc15c
SAGNIK PAL <palsagnik2102@gmail.com>
2024-10-18 15:21:38 +0530

    pkg/models added

--- cmd/odin/main.go
+++ cmd/odin/main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"sync/atomic"
 
+	models "github.com/palSagnik/gobalance/pkg/models"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,45 +15,13 @@ var (
 	port = flag.Int("port", 8080, "port to start lb")
 )
 
-type Service struct {
-	Name     string
-	Replicas []string
-}
-
-// This is a representation of a configuration given to the loadbalancer
-type Config struct {
-	Services []Service
-
-	// Name of the strategy used for load balancing
-	Strategy string
-}
-
-// Server is an instance of a running server
-type Server struct{
-	url *url.URL
-	proxy *httputil.ReverseProxy
-}
-
-type ServerList struct{
-	Servers []*Server
-
-	// The list of servers are circulated through in a cyclic manner
-	// next server is (currentServer + 1) * len(servers)
-	currentServer uint32
-}
-
-func (sl *ServerList) Next() uint32 {
-	nxt := atomic.AddUint32(&sl.currentServer, uint32(1))
-	return nxt % uint32(len(sl.Servers))
-}
-
 type Odin struct {
-	Config *Config
-	ServerList *ServerList
+	Config *models.Config
+	ServerList *models.ServerList
 }
 
-func NewOdin(conf *Config) *Odin {
-	servers := make([]*Server, 0)
+func NewOdin(conf *models.Config) *Odin {
+	servers := make([]*models.Server, 0)
 	for _, service := range conf.Services {
 		for _, replica := range service.Replicas {
 			serverUrl, err := url.Parse(replica)
@@ -62,33 +30,42 @@ func NewOdin(conf *Config) *Odin {
 			}
 
 			serverProxy := httputil.NewSingleHostReverseProxy(serverUrl)
-			servers = append(servers, &Server{
-				url: serverUrl,
-				proxy: serverProxy,
+			servers = append(servers, &models.Server{
+				Url: serverUrl,
+				Proxy: serverProxy,
 			})
 		}
 	}
 
 	return &Odin{
 		Config: conf,
-		ServerList: &ServerList{
+		ServerList: &models.ServerList{
 			Servers: servers,
-			currentServer: uint32(0),
+			CurrentServer: uint32(0),
 		},
 	}
 }
 
 func (o *Odin) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	log.Info("received new request: url = %s", req.URL.String())
+	log.Infof("received new request: url = '%s'", req.Host)
+
 	next := o.ServerList.Next()
-	o.ServerList.Servers[next].proxy.ServeHTTP(res, req)
+	log.Infof("forwarding to server: '%d'", next)
+	o.ServerList.Servers[next].Proxy.ServeHTTP(res, req)
 }
 
 
 func main() {
-
 	flag.Parse()
-	conf := &Config{}
+
+	conf := &models.Config{
+		Services: []models.Service {
+			{
+				Name: "Test",
+				Replicas: []string{"http://localhost:8081", "http://localhost:8082"},
+			},
+		},
+	}
 	odin := NewOdin(conf)
 
 	server := http.Server{
